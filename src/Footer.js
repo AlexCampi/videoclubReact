e47bcf87aa42774aa9ea7a0d54ddbd7e17955ead
Alex Campillo <alexcampillodesign@gmail.com>
2022-07-07 15:50:02 +0200

    videoclub

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer(props){
+    return (
+        <footer>
+            <p>2022© El videoclub de los stormtroopers.</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/Main.js
@@ -0,0 +1,32 @@
+function Cards(props){
+    return(
+        <>
+        <div className="card">
+            <img src={props.cartel} alt={props.titulo} />
+            <div className="container">
+                <h4>{props.titulo}</h4>
+                <p>{props.sinopsis}</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+function Main(props) {
+    
+    return (
+        <main className="catalogo">
+            {props.peliculas.map((pelicula, index) => {
+                return (
+                    <Cards
+                        cartel={pelicula.cartel}
+                        titulo={pelicula.titulo}
+                        sinopsis={pelicula.sinopsis}
+                    />
+                )
+            })}
+        </main>
+    )
+}
+
+export default Main
